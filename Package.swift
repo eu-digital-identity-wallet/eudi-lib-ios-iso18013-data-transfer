a4f854de2c81dd18217a80aaac8fcd35fb0acf9b
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-11-30 02:40:48 +0200

    depend on security package with upToNextMajor

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["MdocDataTransfer18013"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", .upToNextMinor(from: "0.1.0")),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", .upToNextMajor(from: "0.1.0")),
 	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
