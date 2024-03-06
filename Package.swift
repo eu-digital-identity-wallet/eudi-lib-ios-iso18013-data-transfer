83169354d1d531ef9e565e7c259559e766775a45
Filippos Sakellaropoulos <phisakel@gmail.com>
2024-03-06 22:16:39 +0200

    Update dependencies in Package.resolved and Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MdocDataTransfer18013",
-	platforms: [.macOS(.v12), .iOS("13.1")],
+	platforms: [.macOS(.v12), .iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
