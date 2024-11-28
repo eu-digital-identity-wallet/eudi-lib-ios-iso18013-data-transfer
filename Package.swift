e9f4aca463a6beafca8dd28f28f5eb9057d2f811
Filippos Sakellaropoulos <phisakel@gmail.com>
2024-11-28 09:53:38 +0200

    Update package dependencies to point to the eu-digital-identity-wallet security repository 0.3.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
             targets: ["MdocDataTransfer18013"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/niscy-eudiw/eudi-lib-ios-iso18013-security.git", branch: "SecureArea"),
-		//.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", exact: "0.2.6"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", exact: "0.3.0"),
 	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
