3dcfcc093ed31e6b41b89ae4cc654ac0788dae76
Filippos Sakellaropoulos <phisakel@gmail.com>
2025-03-11 12:57:37 +0200

    Update package dependencies to version 0.4.6 for eudi-lib-ios-iso18013-security and 0.5.6 for eudi-lib-ios-iso18013-data-model; update swift-crypto to version 3.11.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["MdocDataTransfer18013"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", exact: "0.4.5"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", exact: "0.4.6"),
 ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
