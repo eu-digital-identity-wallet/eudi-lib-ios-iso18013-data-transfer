99b10d09c4d3f02508b7c63d4907a28972cf74ec
Filippos Sakellaropoulos <phisakel@gmail.com>
2025-10-06 21:34:25 +0300

    Update package dependencies to version 0.7.0 and adjust platform requirements to macOS 14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MdocDataTransfer18013",
-	platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v10)],
+	platforms: [.macOS(.v14), .iOS(.v16), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["MdocDataTransfer18013"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", exact: "0.6.7"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", exact: "0.7.0"),
 	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
