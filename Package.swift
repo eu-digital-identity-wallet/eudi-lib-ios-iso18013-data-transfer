c00b883afc280b8fd5c3af88405e314c261241de
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-05-15 16:14:46 +0300

    Update eudi-lib-ios-iso18013-security dependency to version 0.20.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,9 @@ let package = Package(
             targets: ["MdocDataTransfer18013"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", exact: "0.15.0"),
-	],
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-security.git", exact: "0.20.0"),
+		.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
