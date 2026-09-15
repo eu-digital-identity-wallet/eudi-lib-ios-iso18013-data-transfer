3a56ecc0411c65c48bd2d44ded97934c75d8f121
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-09-15 11:17:31 +0300

    Update minimum platform versions to macOS 15 and iOS 17 in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MdocDataTransfer18013",
-	platforms: [.macOS(.v14), .iOS(.v16), .watchOS(.v10)],
+	platforms: [.macOS(.v15), .iOS(.v17), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
